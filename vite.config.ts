fde90c98cae3ed943442f7440efbd801bc273169
Matheus Bajo <matheusbajo@gmail.com>
2025-06-16 12:04:18 -0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/aluguel-de-games-proj/', // Substitua pelo nome do seu repo
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // Configuração para GitHub Pages
+})
